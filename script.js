3b646e8b73690b375fbd19b3fbfd70cde370a319
Joseph Hezkiah <josephkeswet@gmail.com>
2022-05-20 04:47:23 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// const form = document.getElementById('form')
+// const firstname = document.getElementById('user-firstname')
+// const lastname = document.getElementById('user-lastname')
+// const email = document.getElementById('user-mail')
+// const password = document.getElementById('user-password')
+
+// form.addEventListener('submit',(e)=>{
+//     e.preventDefault();
+
+//     checkInputs();
+// });
+
+// function checkInputs() {
+//     //get the values from the inputs
+
+//    const firstNameValue = firstname.value
+//     const lastNameValue = lastname.value
+//     const emailValue = email.value
+//     const passwordValue = password.value
+// }
